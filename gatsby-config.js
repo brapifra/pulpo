b18d5c67a38efbfd137f9f6a8e320d09a4b22e6d
Brais Piñeiro <brapifra@gmail.com>
2020-05-07 00:38:17 +0100

    Correct siteMetadata title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Node.js (TypeScript) API',
+    title: 'Pulpo - Personal github statistics',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
