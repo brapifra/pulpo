3cb3228301edfeb5092223d16bf9c9c3ab8a79e2
Brais Piñeiro <brapifra@gmail.com>
2020-05-07 01:04:46 +0100

    Add basic 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../layouts/Main';
+import Logo from '../components/Logo';
+
+export default () => {
+  return (
+    <Main>
+      <Logo />
+      <h1>Not found</h1>
+    </Main>
+  )
+}
