c7c25bcbba6f21c16369f9f7bc9c3725c91a75f7
Brais Piñeiro <brapifra@gmail.com>
2020-05-09 10:50:11 +0100

    Make Card component more accessible

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,13 +6,25 @@ interface Props {
 }
 
 export default ({ title, description }: Props) => {
+  const formattedTitle = title.toLocaleString("en");
   return (
     <div
       className="card"
       style={{ minWidth: 0, boxSizing: "border-box", height: 166 }}
     >
       <h4 style={{ marginBlockStart: "1.15em", marginBlockEnd: "1.15em" }}>
-        <b style={{ fontWeight: "bold" }}>{title.toLocaleString("en")}</b>
+        <b
+          style={{
+            fontWeight: "bold",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            display: "block",
+          }}
+          title={formattedTitle}
+        >
+          {formattedTitle}
+        </b>
       </h4>
       <p>{description}</p>
     </div>
